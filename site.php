655dd59298b217afd99200f6971d56174c37cfbd
Silvia Rocha <silviadprocha@gmail.com>
2020-02-28 21:30:56 +0900

    data types

--- site.php
+++ site.php
@@ -6,16 +6,12 @@
   </head>
   <body>
 
-    <?php
-    $characterName = "Tom"; #variable
-    $characterAge = 80; #variable
-    echo "There once was a man named $characterName <br>";
-    echo "He was $characterAge years old <br>";
-    $characterName = "Mike";
-    echo "He really liked the name $characterName <br>";
-    echo "But didn't like being $characterAge <br>";
+<?php
+  $phrase = "To be or not to be";
+  $age = 30;
+  $isMale = false;
+  echo 4.57;
 
-    ?>
-
+?>
   </body>
 </html>
