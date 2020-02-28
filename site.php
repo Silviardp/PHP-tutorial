7042b8ad3681221bd806edbd8c68585c318d1f87
Silvia Rocha <silviadprocha@gmail.com>
2020-02-28 19:03:57 +0900

    setup and writing html

---
+++ site.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  <body>
+    <?php
+       echo "<h1>Silvia's site</h1>";
+       echo "<hr>";
+       echo "<p>This is my site</p>";
+      ?>
+  </body>
+</html>
