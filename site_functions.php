56e35f44d7b48ed8c52230a08810b87df8798942
Silvia Rocha <silviadprocha@gmail.com>
2020-03-01 21:59:13 +0900

    functions

---
+++ site_functions.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  <body>
+
+  <?php
+    function sayHi($name, $age){
+      echo "Hello $name, you are $age <br>";
+    }
+
+    sayHi("Silvia", 20);
+    sayHi("Tom", 30);
+    sayHi("Dave", 40);
+    sayHi("Oscar", 50);
+  ?>
+
+  </body>
+</html>
