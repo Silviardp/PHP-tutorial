9546ca194d9b779ba4c271b6459e6b371d25d759
Silvia Rocha <silviadprocha@gmail.com>
2020-03-01 21:27:41 +0900

    arrays

--- site_array.php
+++ site_array.php
@@ -6,6 +6,12 @@
   </head>
   <body>
 
+  <?php
+    $friends = array("Kevin", "Mike", "David", "Silvia");
+    $friends[4] = "Steve";
+    echo $friends[4];
+    echo count($friends);
+  ?>
 
   </body>
 </html>
