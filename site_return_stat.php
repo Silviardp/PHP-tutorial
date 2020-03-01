3fa1479a3b4dfe7d650af2e6307b5401edd93a43
Silvia Rocha <silviadprocha@gmail.com>
2020-03-01 22:24:32 +0900

    retutn statement

---
+++ site_return_stat.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  <body>
+
+  <?php
+    function cube($num) {
+      return $num * $num * $num;
+    }
+
+    echo $cubeResult = cube(4);
+
+  ?>
+
+  </body>
+</html>
