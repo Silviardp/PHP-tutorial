36a821cca8e980a0faabaeb6ecbfb62f8f1cadae
Silvia Rocha <silviadprocha@gmail.com>
2020-03-02 15:15:54 +0900

    include PHP

---
+++ site_php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  <body>
+
+  <?php
+    $title = "My first post";
+    $author = "Silvia";
+    $wordCount = 400;
+    include "article-header.php";
+    include "useful-tools.php";
+    sayHi("Silvia <br>");
+    echo $feetInMile;
+  ?>
+  </body>
+</html>
