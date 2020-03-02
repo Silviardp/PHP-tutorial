36acbc6063e084a3ec9aea835e03228cdf5010e8
Silvia Rocha <silviadprocha@gmail.com>
2020-03-02 12:37:38 +0900

    while and do while loops

---
+++ site_while.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  <body>
+
+  <?php
+    // $index = 1;
+    // while($index <= 5){
+    //   echo "$index <br>";
+    //   $index++;
+    // }
+
+    $indexx = 6;
+    do{
+      echo "$indexx <br>";
+      $indexx ++;
+    } while ($indexx <= 5);
+  ?>
+
+  </body>
+</html>
