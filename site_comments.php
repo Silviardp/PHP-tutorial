5bcc06ed40e8815908c2a1ec29420b67d0c4f05d
Silvia Rocha <silviadprocha@gmail.com>
2020-03-02 13:02:32 +0900

    comments

---
+++ site_comments.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+  </head>
+  <body>
+
+  <?php
+    // This is a comment
+    /* This is a multiple comment
+    line
+    line
+    */
+    echo "Comments are fun";
+  ?>
+
+  </body>
+</html>
